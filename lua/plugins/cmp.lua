524de27e1cfeda3bc1069d69885db8b469f80940
hhannin <hanyinhui1191@gmail.com>
2024-06-05 23:55:52 +0800

    feat

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,21 +1,87 @@
 return {
-  'hrsh7th/nvim-cmp',  -- 代码补全
-  dependencies = {
-    'hrsh7th/cmp-nvim-lsp',  -- LSP 源
-    'hrsh7th/cmp-buffer',    -- 缓冲区源
-  },
-  config = function()
-    local cmp = require('cmp')
-    cmp.setup({
-      mapping = {
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      },
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-      },
-    })
-  end
-}
+  -- nvim-cmp 自动补全插件
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0
+          and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
+            == nil
+      end
+
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              feedkey("<Plug>luasnip-expand-or-jump", "")
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              feedkey("<Plug>luasnip-jump-prev", "")
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,41 @@
 return {
   "neovim/nvim-lspconfig", -- LSP 配置
   config = function()
-    require("lspconfig").clangd.setup({}) -- 配置 clangd 作为 LSP 服务器
+    local lspconfig = require("lspconfig")
+    lspconfig.clangd.setup({
+      handlers = {
+        ["textDocument/publishDiagnostics"] = vim.lsp.with(
+          vim.lsp.diagnostic.on_publish_diagnostics,
+          {
+            -- 显示的诊断数
+            virtual_text = true, -- 不显示虚拟文本
+            signs = true, -- 在行号栏显示标志
+            update_in_insert = true, -- 插入模式下不更新
+          }
+        ),
+      },
+    }) -- 配置 clangd 作为 LSP 服务器
+    -- LSP 快捷键配置
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+
+    -- 代码跳转
+    map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+
+    -- 代码操作
+    map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+
+    -- 代码操作
+    map("n", "<leader>ac", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+
+    -- 诊断信息
+    map("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    map("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    map("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
   end,
 }
